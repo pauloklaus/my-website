98772ee1c823bc1d620fa58de38ee09c7880c888
Paulo Sergio Klaus <contato@pauloklaus.com.br>
2021-10-08 23:58:50 -0300

    feat: Add vuejs framework

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{ appName }}
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    appName() {
+      return process.env.VUE_APP_NAME;
+    },
+  }
+}
+</script>
+
+<style scoped>
+div {
+  background-color: #003;
+  color: #666;
+  font-size: 3rem;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>

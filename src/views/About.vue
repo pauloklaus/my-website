93069c36c41fb571f940b51055fba23fbdf278a6
Paulo Sergio Klaus <contato@pauloklaus.com.br>
2022-01-24 07:34:40 -0300

    fix: Design fix in the About space

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,34 +1,39 @@
 <template>
-  <div>
-    <p class="text-white">Hi! This is my personal homepage.</p>
+  <b-row class="mt-2">
+    <b-col sm="12" md="5">
+      <p class="text-white">Hi! This is my personal homepage.</p>
 
-    <p>
-      I work with software development since 1998.
-      In 2002 I started creating web applications using PHP/HTML/CSS/JS.
-      I started using Bootstrap a few years later to speed up the creation of interfaces.
-      I followed the rise of Angular and React.
-      <span class="text-white">In 2018 I got in touch with the VueJS framework.</span>
-      I started working with componentization in VueJS and this has been a differential for
-      structuring large applications, applying good code writing practices in addition to
-      facilitating the creation of cleaner interfaces, easier to test, maintain and grow.
-    </p>
+      <p>
+        I work with software development since 1998.
+        In 2002 I started creating web applications using PHP/HTML/CSS/JS.
+        I started using Bootstrap a few years later to speed up the creation of interfaces.
+        I followed the rise of Angular and React.
+        <span class="text-white">In 2018 I got in touch with the VueJS framework.</span>
+        I started working with componentization in VueJS and this has been a differential for
+        structuring large applications, applying good code writing practices in addition to
+        facilitating the creation of cleaner interfaces, easier to test, maintain and grow.
+      </p>
+    </b-col>
 
-    <p>
-      I feel like a <span class="text-white">Linux enthusiast</span>.
-      I've been following the evolution for many years and using it as my <span class="text-white">main</span> operating system.
-    </p>
+    <b-col sm="12" md="7">
+      <p>
+        I feel like a <span class="text-white">Linux enthusiast</span>.
+        I've been following the evolution for many years and using it as my <span class="text-white">main</span> operating system.
+      </p>
 
-    <p>
-      Above, some links to my social networks and resources I keep available.
-    </p>
+      <p>
+        Above, some links to my social networks and resources I keep available.
+      </p>
 
-    <p>
-      In the playground beside it's possible to test some simple components that I made
-      available in the npm repository.
-    </p>
+      <p>
+        In the playground beside it's possible to test some simple components that I made
+        available in the npm repository.
+      </p>
 
-    <p>
-      Some skills:
+      <p>
+        Some skills:
+      </p>
+
       <ul>
         <li><span class="text-white">#javascript</span> <span class="text-white">#vuejs</span> #jest #vuetest #axios #apirest #python #php</li>
         <li>#html #css #bootstrap</li>
@@ -36,8 +41,8 @@
         <li><span class="text-white">#linux</span> #shellscript #bash #github #docker</li>
         <li>#cleancode #cleanarchitecture #codereview #pairprogramming</li>
       </ul>
-    </p>
-  </div>
+    </b-col>
+  </b-row>
 </template>
 
 <style scoped>
